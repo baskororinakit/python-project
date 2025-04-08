057fd9c027cd1e8da628fb96c8ff4289c69e1bc7
Baskoro Rinakit <baskororinakit03@gmail.com>
2025-04-08 12:27:27 +0700

    first commit

---
+++ project/age_calculator.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+tanggal_lahir = input ("Masukkan tanggal lahir anda (format: YYYY-MM-DD): ")
+tgl_lahir = datetime.strptime(tanggal_lahir, "%Y-%m-%d")
+hari_ini = datetime.now()
+umur = hari_ini - tgl_lahir
+
+umur_tahun = umur.days // 365
+sisa_hari = umur.days % 365
+umur_bulan = sisa_hari // 30
+umur_hari = sisa_hari % 30  
+
+print(f"\nUmur kamu sekarang : {umur_tahun} tahun, {umur_bulan} bulan, {umur_hari} hari")
+print(f"Kamu sudah hidup selama {umur.days} hari")
