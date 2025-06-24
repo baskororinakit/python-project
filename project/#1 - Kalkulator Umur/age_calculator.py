e4347cbfe73509ea46c3058f9280bafa7dcaab8a
Baskoro Rinakit <baskororinakit03@gmail.com>
2025-06-24 22:54:22 +0700

    Update variabel name

--- project/#1 - Kalkulator Umur/age_calculator.py
+++ project/#1 - Kalkulator Umur/age_calculator.py
@@ -5,10 +5,10 @@ tgl_lahir = datetime.strptime(tanggal_lahir, "%Y-%m-%d")
 hari_ini = datetime.now()
 umur = hari_ini - tgl_lahir
 
-umur_tahun = umur.days // 365
+year_age = umur.days // 365
 sisa_hari = umur.days % 365
 umur_bulan = sisa_hari // 30
 umur_hari = sisa_hari % 30  
 
-print(f"\nUmur kamu sekarang : {umur_tahun} tahun, {umur_bulan} bulan, {umur_hari} hari")
+print(f"\nUmur kamu sekarang : {year_age} tahun, {umur_bulan} bulan, {umur_hari} hari")
 print(f"Kamu sudah hidup selama {umur.days} hari")
